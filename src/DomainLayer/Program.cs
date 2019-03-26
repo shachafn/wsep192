87384a2f83b9a540596cb9bd9df1860cac9d182c
shachafn <nahmish@post.bgu.ac.il>
2019-03-26 21:29:19 +0200

    Added ServiceLayer and DomainLayer projects

---
+++ src/DomainLayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DomainLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

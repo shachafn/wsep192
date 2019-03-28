4791395dc298d2dd323d566f8a95409c2c137598
Hassonbe <hassonbe@post.bgu.ac.il>
2019-03-28 15:55:04 +0200

    -ShopOwner : implemented some functions - User: added fields to detrmine if admin , and shops owned

--- src/DomainLayer/ShopOwner.cs
+++ src/DomainLayer/ShopOwner.cs
@@ -1,25 +1,174 @@
-public class ShopOwner
+using System;
+using System.Collections.Generic;
+
+namespace DomainLayer
 {
-    // Constructor that takes no arguments:
-    public ShopOwner()
+    public class ShopOwner
     {
+        public Dictionary<string, List<ShopOwner>> shopOwners = new Dictionary<string, List<ShopOwner>>();// will hold all the current shopOwners
+
+        private User owner; // may be  a list of owners is needed
+        private Shop shop;
+        private List<ShopOwner> ownersAssigned;
+        // according to our design : manager is an owner with limited actions availabe to him
+       //rivate List<ShopOwner> managersAssigned; i dont think we  need it but need to talk about it
+        private ManagingPrivileges privileges;
+
+        public ShopOwner(User owner,Shop shop,bool manager=false)
+        {
+            this.owner = owner;
+            this.shop = shop;
+            ownersAssigned = new List<ShopOwner>();
+            managersAssigned = new List<ShopOwner>();
+            this.privileges = new ManagingPrivileges(manager);
+        }
+        /// <summary>
+        /// get a shopOwner instance from the current shopOwners in the system
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="shop"></param>
+        /// <returns> a relvant shopOwner if one exists , null otherwise</returns>
+        public ShopOwner GetShopOwner(User user, Shop shop)
+        {
+            bool exists = user.isLogged() && shopOwners.ContainsKey(user.username);
+            if(!exists)
+            {
+                return null;
+            }
+            foreach(ShopOwner shopOnwer in shopOwners[user.username])
+            {
+                if(shopOnwer.shop.Equals(shop))
+                {
+                    return shopOnwer;
+                }
+            }
+            return null;
+        }
+
+        public bool addManager(User newManager,ManagingPrivileges privileges)
+        {
+            if (!this.privileges.hasPrivilege("addManager"))
+            {
+                return false;
+            }
+            this.shop.addOwner(newManager);
+            ShopOwner newShopOwner = new ShopOwner(newManager,this.shop,true);
+            newShopOwner.privileges = privileges;
+            ownersAssigned.Add(newShopOwner);
+            ownersDictAdd(newManager.username,newShopOwner);
+            return true;
+        }
+
+        public bool addOwner(User newManager)
+        {
+            if (!this.privileges.hasPrivilege("addOwner"))
+            {
+                return false;
+            }
+            this.shop.addOwner(newManager);
+            ShopOwner newShopOwner = new ShopOwner(newManager, this.shop, false);
+            ownersAssigned.Add(newShopOwner);
+            ownersDictAdd(newManager.username, newShopOwner);
+            return true;
+        }
+
+        private void ownersDictAdd(string username,ShopOwner newshopOwner)
+        {
+            if(shopOwners.ContainsKey(username))
+            {
+                shopOwners[username].Add(newshopOwner);
+            }
+            else
+            {
+                List<ShopOwner> userShops =new List<ShopOwner>();
+                userShops.Add(newshopOwner);
+                shopOwners.Add(username, userShops);
+            }
+        }
+
+        /// <summary>
+        /// removes a manger or a owner you assigned
+        /// </summary>
+        public bool removeOwner(ShopOwner toRemove)
+        {
+            if (!this.privileges.hasPrivilege("addOwner")|| !ownersAssigned.Contains(toRemove))
+            {
+                return false;
+            }
+            //first remove all of the owners/managers assigned by this owner
+            foreach (ShopOwner assigned in toRemove.ownersAssigned)
+            {
+                removeOwner(assigned);
+            }
+            ownersAssigned.Remove(toRemove);
+            ownersDictRemove(toRemove.owner.username, toRemove);
+            return true;
+        }
+
+        private void ownersDictRemove(string username, ShopOwner toRemove)
+        {
+            List<ShopOwner> ownedShops = shopOwners[username];
+            if(ownedShops.Count == 1)
+            {
+                shopOwners.Remove(username);
+            }
+            else
+            {
+                ownedShops.Remove(toRemove);
+            }
+        }
+
+        public void closeShop()
+        {
+
+        }
+        // Method that overrides the base class (System.Object) implementation.
+        public override string ToString()
+        {
+            return "";
+        }
+
+        public class ManagingPrivileges
+        {
+            List<string> allowedActions { get => allowedActions; set => allowedActions = value; }
+            bool isManager; // if not a manager it is an owner , an owner has all the actions available 
+            public ManagingPrivileges(List<string> allowedActions)
+            {
+                this.allowedActions = allowedActions;
+                this.isManager = true;
+            }
+            public ManagingPrivileges(bool isManager)
+            {
+                this.allowedActions = new List<string>();
+                this.isManager = isManager;
+            }
+
+            public bool addAction(string action)
+            {
+                bool exsits = allowedActions.Contains(action);
+                if(!exsits)
+                {
+                    allowedActions.Add(action);              
+                }
+                return !exsits;
+            }
+
+            public bool removeAction(string action)
+            {
+                bool exsits = allowedActions.Contains(action);
+                if (exsits)
+                {
+                    allowedActions.Remove(action);
+                }
+                return exsits;
+            }
+
+            public bool hasPrivilege(string action)
+            {
+
+                return !isManager || allowedActions.Contains(action);
+            }
+        }
     }
 
-    public void addManager()
-    {
-
-    }
-    public void removeManager()
-    {
-
-    }
-    public void closeShop()
-    {
-
-    }
-    // Method that overrides the base class (System.Object) implementation.
-    public override string ToString()
-    {
-    }
 }
-
--- src/DomainLayer/User.cs
+++ src/DomainLayer/User.cs
@@ -8,28 +8,54 @@ namespace DomainLayer
         public static Dictionary<string, User> users = new Dictionary<string, User>(); // a list of all registered users in the current session
 
         private bool logged;
-        private string username;
+        public string username { get => username; private set => username=value; }
         private string passHash;
         private ShoppingBag currentBag;
         private List<ShoppingBag> purchaseHistory;
         private UserInfo userInfo;
-
+        private List<Shop> shopsOwned;
+        private bool isAdmin; 
         // Constructor that takes no arguments:
         public User()
         {
             logged = false;
+            username = "";
+            passHash = "";
+            currentBag = new ShoppingBag();
+            purchaseHistory = new List<ShoppingBag>();
+            userInfo = new UserInfo();
+            shopsOwned = new List<Shop>();
+            isAdmin = false;
         }
 
         public User(string username, string password)
         {
             this.username = username;
             this.passHash = GetStringSha256Hash(password);
+            logged = false;
+            currentBag = new ShoppingBag();
+            purchaseHistory = new List<ShoppingBag>();
+            userInfo = new UserInfo();
+            shopsOwned = new List<Shop>();
+            isAdmin = false;
         }
 
+        internal bool isLogged()
+        {
+            return logged;
+        }
+
+        /// <summary>
+        /// logs the user in , changes its logged field to true , and retrieves all it's stored information
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>true if the login was sucseesful, false if one or more of the parameters were wrong 
+        /// or if the user is already connected</returns>
         bool login(string username, string password)
         {
             // check if the user exist and the password is correct 
-            if (!users.ContainsKey(username) || users[username].checkPass(password))
+            if (!users.ContainsKey(username) || users[username].checkPass(password) || users[username].logged)
             {
                 return false;
             }
@@ -39,9 +65,9 @@ namespace DomainLayer
             return true;
         }
 
-        /* 
-         * this method logs the user out , and saves it's changed properties
-         * returns : false if the user is not logged in , true otherwise
+        /** 
+         *<summary>this method logs the user out , and saves it's changed properties</summary> 
+         * <returns>false if the user is not logged in , true otherwise</returns> 
          */
         bool logout()
         {
@@ -54,11 +80,13 @@ namespace DomainLayer
         }
 
 
-        /*
-         * this method recieves : a  username and  a password 
-         * if the username is not taken already , the method creates a new user with this credentials and stores it in the users list
-         * returns the created user or null otherwise
-         */
+        /// <summary>
+        ///  if the username is not taken already ,
+        ///  the method creates a new user with this credentials and stores it in the users list
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns> returns the created user or null otherwise</returns>
         public static User Register(string username, string password)
         {
             if (users.ContainsKey(username))
@@ -69,6 +97,12 @@ namespace DomainLayer
             users.Add(username, newUser);
             return newUser;
         }
+        /// <summary>
+        /// searches products according to a user recived string and filters them if a filter is given
+        /// </summary>
+        /// <param name="searchString"></param>
+        /// <param name="filters"> an optional argument , the results of the search will be filtered using all of these</param>
+        /// <returns>returns the list of products found (an empty list if found none)</returns>
         List<Product> search(string searchString, List<ProductFilter> filters = null)
         {
             List<Product> productsFound = new List<Product>();
@@ -102,6 +136,7 @@ namespace DomainLayer
             this.currentBag = user.currentBag;
             this.purchaseHistory = user.purchaseHistory;
             this.userInfo = user.userInfo;
+            this.shopsOwned = user.shopsOwned;
         }
 
         private void saveUserChanges()
@@ -110,6 +145,7 @@ namespace DomainLayer
             savedUser.currentBag = this.currentBag;
             savedUser.purchaseHistory = this.purchaseHistory;
             savedUser.userInfo = this.userInfo;
+            savedUser.shopsOwned = this.shopsOwned;
         }
         //a method that is used to hash the password
         internal static string GetStringSha256Hash(string text)

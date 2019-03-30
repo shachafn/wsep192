69b4788b78f2993351ae0fec1938dbef89316c77
Liron Abraham <lironabr@post.bgu.ac.il>
2019-03-30 18:35:24 +0300

    namespace, getters and setters

--- src/DomainLayer/ShopProduct.cs
+++ src/DomainLayer/ShopProduct.cs
@@ -1,21 +1,25 @@
-﻿public class ShopProduct
+﻿namespace DomainLayer
 {
-    private Product _product;
-    private double _price;
-    private int _quantity;
-    public ShopProduct(Product product, double price, int quantity)
-    {
-        this.Product = product;
-        this.Price = price;
-        this.Quantity = quantity;
-    }
-    public int Quantity { get; set; }
-    public double Price { get; set; }
-    public Product Product { get; }
 
-    public override string ToString()
+    public class ShopProduct
     {
-        return "Product: " + _product.ToString() + "\nPrice: " + _price.ToString() + "\nQuantity: " + _quantity.ToString();
+        private Product _product;
+        private double _price;
+        private int _quantity;
+        public ShopProduct(Product product, double price, int quantity)
+        {
+            this.Product = product;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; }
+
+        public override string ToString()
+        {
+            return "Product: " + _product.ToString() + "\nPrice: " + _price.ToString() + "\nQuantity: " + _quantity.ToString();
+        }
     }
+
 }
-

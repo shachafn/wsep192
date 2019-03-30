baa988dad4650820e248adbca9a89b7ac5897386
Liron Abraham <lironabr@post.bgu.ac.il>
2019-03-30 18:41:29 +0300

    ShoppingBag class implemented

--- src/DomainLayer/ShoppingBag.cs
+++ src/DomainLayer/ShoppingBag.cs
@@ -1,14 +1,39 @@
-public class ShoppingBag
+using System.Collections.Generic;
+
+namespace DomainLayer
 {
-    // Constructor that takes no arguments:
-    public ShoppingBag()
+
+    public class ShoppingBag
     {
+        private List<ShoppingCart> _carts;
+        public ShoppingBag()
+        {
+            _carts = new List<ShoppingCart>();
+        }
+        public List<ShoppingCart> Carts { get; set; }
+        public void AddCart(ShoppingCart newCart)
+        {
+            _carts.Add(newCart);
+        }
+        public bool HasProduct(Product p)
+        {
+            foreach (ShoppingCart cart in Carts)
+            {
+                if (cart.HasProduct(p))
+                    return true;
+            }
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (ShoppingCart cart in Carts)
+            {
+                result += "\n" + cart.ToString();
+            }
+            return result;
+        }
     }
 
-
-    // Method that overrides the base class (System.Object) implementation.
-    public override string ToString()
-    {
-    }
 }
-

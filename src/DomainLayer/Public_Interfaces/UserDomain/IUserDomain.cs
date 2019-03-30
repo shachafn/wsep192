18355692c319f490236eb3478b2a04d83acab370
unknown <nahmish@post.bgu.ac.il>
2019-03-30 16:39:39 +0300

    Added IUserDomain interface and aggregate classes

---
+++ src/DomainLayer/Public_Interfaces/UserDomain/IUserDomain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace DomainLayer.Public_Interfaces.UserDomain
+{
+    interface IUserDomain
+    {
+        /// <summary>
+        /// Registers a user using the registration info.
+        /// </summary>
+        /// <param name="info">Info of the user to register</param>
+        /// <returns>True if registered successfully. False otherwise, with an error message as out parameter.</returns>
+        bool RegisterUser(UserRegistrationInfo info, out string ErrorMessage);
+
+        /// <summary>
+        /// Logins a user using the username and password.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>True if it could login successfully. False otherwise.</returns>
+        bool Login(string username, string password);
+
+        /// <summary>
+        /// Logins a user using the mailAddress and password.
+        /// </summary>
+        /// <param name="mailAddress"></param>
+        /// <param name="password"></param>
+        /// <returns>True if it could login successfully. False otherwise.</returns>
+        bool Login(MailAddress mailAddress, string password);
+
+        /// <summary>
+        /// Logs out the user.
+        /// </summary>
+        /// <param name="username"></param>
+        void Logout(string username);
+
+        /// <summary>
+        /// Opens a store for the user.
+        /// </summary>
+        /// <param name="errorMessage"></param>
+        /// <returns>True if opened successfully. False otherwise, with an error message as out parameter.</returns>
+        bool OpenStore(out string errorMessage);
+
+        /// <summary>
+        /// Returns a list of all product purchases info.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns>The list.</returns>
+        IEnumerable<ProductPurchaseHistory> WatchHistory(string username);
+
+        /// <summary>
+        /// Replaces the user's profile info with a new profile info.
+        /// </summary>
+        /// <param name="newProfileInfo"></param>
+        /// <param name="errorMessage"></param>
+        /// <returns>True if replaced successfully. False otherwise, with an error message as an out parameter.</returns>
+        bool EditProfile(ProfileInfo newProfileInfo, out string errorMessage);
+    }
+}
---
+++ src/DomainLayer/Public_Interfaces/UserDomain/ProductPurchaseHistory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLayer.Public_Interfaces.UserDomain
+{
+    class ProductPurchaseHistory
+    {
+    }
+}

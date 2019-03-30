152916b90158ef1295662eafa7c0f15645a6ca98
Liron Abraham <lironabr@post.bgu.ac.il>
2019-03-30 16:22:03 +0300

    Formatting
    Changed name of private fields, fixed functions names
    added getters and setters and "Equals" function.
    added using DomainLayer.

--- src/DomainLayer/Product.cs
+++ src/DomainLayer/Product.cs
@@ -1,3 +1,4 @@
+using DomainLayer;
 using System;
 using System.Collections.Generic;
 
@@ -5,46 +6,62 @@ using System.Collections.Generic;
 public class Product
 {
     private const int MaxReviewLength = 250;
-    private string name;
-    private string category;
-    private double price;
-    private double rate;
-    private int sumOfRates;
-    private int numberOfRates;
-    private Dictionary<User, string> reviews;
+    private string _name;
+    private string _category;
+    private double _rate;
+    private int _sumOfRates;
+    private int _numberOfRates;
+    private Dictionary<User, string> _reviews;
 
 
-     public Product(string name, string category, double price)
+    public Product(string name, string category)
     {
-        this.name=name;
-        this.category= category;
-        this.price= price;
-        this.reviews =new Dictionary<User, string>();
-        this.rate=0;
+        this._name = name;
+        this._category = category;
+        _reviews = new Dictionary<User, string>();
+        _rate = 0;
+        _sumOfRates = 0;
+        _numberOfRates = 0;
     }
 
 
-    public void addReview(User user, string review)
+    public void AddReview(User user, string review)
     {
-        if(canRateProduct(user) && review.Length <= MaxReviewLength)
-            reviews.Add(user,review);
+        if (canRateProduct(user) && review.Length <= MaxReviewLength)
+            _reviews.Add(user, review);
     }
 
-    public void rateProduct(User user, int rate){
-        if(canRateProduct(user) && rate>0 && rate <= 5){
-            sumOfRates+=rate;
-            numberOfRates++;
-            rate=sumOfRates/numberOfRates;
+    public void RateProduct(User user, int rate)
+    {
+        if (canRateProduct(user) && rate >= 1 && rate <= 5)
+        {
+            _sumOfRates += rate;
+            _numberOfRates++;
+            rate = _sumOfRates / _numberOfRates;
         }
     }
 
-    private bool canRateProduct(User user){
-        return user.isLogged() && user.hasPurchasedProduct(this);
+    private bool canRateProduct(User user)
+    {
+        return user.IsLogged() && user.HasPurchasedProduct(this);
     }
     // Method that overrides the base class (System.Object) implementation.
     public override string ToString()
     {
-        return "Name: "+ name+ "\nCategory: "+category+ "\nRating: "+ rate+ "\nPrice"+price;
+        return "Name: " + Name + "\nCategory: " + Category + "\nRating: " + Rate;
     }
+
+    public string Name { get; set; }
+    public double Rate { get; set; }
+    public string Category { get; set; }
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is Product)) return false;
+        Product p = obj as Product;
+        if (p.Name.Equals(this.Name) && p.Category.Equals(this.Category)) return true;
+        return false;
+    }
+
 }
 

6dfdc376d6e1d04f664ee5e8a80d9923a597c625
Liron Abraham <lironabr@post.bgu.ac.il>
2019-03-30 16:33:05 +0300

    Added ToString()

--- src/DomainLayer/ShopProduct.cs
+++ src/DomainLayer/ShopProduct.cs
@@ -3,13 +3,19 @@
     private Product _product;
     private double _price;
     private int _quantity;
-	public ShopProduct(Product product, double price, int quantity)
-	{
+    public ShopProduct(Product product, double price, int quantity)
+    {
         this.Product = product;
         this.Price = price;
         this.Quantity = quantity;
-	}
+    }
     public int Quantity { get; set; }
     public double Price { get; set; }
     public Product Product { get; }
+
+    public override string ToString()
+    {
+        return "Product: " + _product.ToString() + "\nPrice: " + _price.ToString() + "\nQuantity: " + _quantity.ToString();
+    }
 }
+

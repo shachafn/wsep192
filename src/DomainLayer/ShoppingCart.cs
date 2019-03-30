80f93605d0c509381dd1aeeb3fda535c697bd170
Liron Abraham <lironabr@post.bgu.ac.il>
2019-03-30 18:40:51 +0300

    ShoppingCart class implemented

--- src/DomainLayer/ShoppingCart.cs
+++ src/DomainLayer/ShoppingCart.cs
@@ -1,26 +1,91 @@
-public class ShoppingCart
+using System.Collections.Generic;
+
+
+namespace DomainLayer
 {
-    // Constructor that takes no arguments:
-    public ShoppingCart()
-    {
-    }
 
-    public void addProduct()
+    public class ShoppingCart
     {
 
-    }
-    public void removeProduct()
-    {
+        private Shop _shop;
+        private List<ShopProduct> _shopProducts;
 
-    }
-    public void getAllProducts()
-    {
+        public ShoppingCart(Shop shop)
+        {
+            _shop = shop;
+            _shopProducts = new List<ShopProduct>();
+        }
+        public Shop Shop { get; }
+        public List<ShopProduct> ShopProducts { get; set; }
 
+        public void AddProduct(ShopProduct newShopProduct)
+        {
+            bool product_exists = false;
+            foreach (ShopProduct sp in ShopProducts)
+            {
+                if (sp.Product.Equals(newShopProduct.Product))
+                {
+                    product_exists = true;
+                    sp.Quantity += newShopProduct.Quantity;
+                }
+            }
+            if (!product_exists)
+                _shopProducts.Add(newShopProduct);
+        }
+        public void RemoveProduct(Product p, int amountToRemove = int.MaxValue)
+        {
+            foreach (ShopProduct sp in ShopProducts)
+            {
+                if (sp.Product.Equals(p))
+                {
+                    if (sp.Quantity <= amountToRemove)
+                    {
+                        ShopProducts.Remove(sp);
+                    }
+                    else
+                    {
+                        sp.Quantity -= amountToRemove;
+                    }
+                    break;
+                }
+            }
+        }
+        public bool HasProduct(Product p)
+        {
+            foreach (ShopProduct sp in ShopProducts)
+            {
+                if (sp.Product.Equals(p))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public List<ShopProduct> GetAllProducts()
+        {
+            return ShopProducts;
+        }
+        public double CalculateTotalPrice()
+        {
+            //TODO: next version apply here the discount policy 
+            double total = 0;
+            foreach (ShopProduct sp in ShopProducts)
+            {
+                total += (sp.Quantity * sp.Price);
+            }
+            return total;
+        }
+
+
+        public override string ToString()
+        {
+            string result = Shop.Owner.ToString();
+            foreach (ShopProduct sp in ShopProducts)
+            {
+                result += "\n" + sp.ToString();
+            }
+            return result;
+        }
     }
 
-    // Method that overrides the base class (System.Object) implementation.
-    public override string ToString()
-    {
-    }
 }
-

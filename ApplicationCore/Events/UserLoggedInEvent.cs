679e7b2e994887ec55f7ee56e5917d3b5c4359ee
Shahaf Nahmias <nahmish@post.bgu.ac.il>
2019-05-06 21:51:37 +0300

    Added Login Notification
    Nice to have
    Will fail on a common use case if the notifications mechanism fails

---
+++ ApplicationCore/Events/UserLoggedInEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ApplicationCore.Entities.Users;
+using ApplicationCore.Entitites;
+
+namespace ApplicationCore.Events
+{
+    public class UserLoggedInEvent : IUpdateEvent
+    {
+        public Guid UserGuid { get; private set; }
+
+        public UserLoggedInEvent(Guid userGuid)
+        {
+            UserGuid = userGuid;
+        }
+
+        public string GetMessage()
+        {
+            return "Welcome!";
+        }
+
+        public ICollection<Guid> GetTargets(ICollection<Shop> shops, ICollection<BaseUser> registeredUsers)
+        {
+            ICollection<Guid> result = new List<Guid>();
+
+            result.Add(UserGuid);
+
+            return result;
+        }
+    }
+}

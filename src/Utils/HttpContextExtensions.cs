3b647b24444842edcc7b7a8b7694a2dbc4b549b4
Shahaf Nahmias <nahmish@post.bgu.ac.il>
2019-05-07 20:22:12 +0300

    Added Utils Extension Method GetSessionId for HttpContext

---
+++ src/Utils/HttpContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+    public static class HttpContextExtensions
+    {
+        public static Guid GetSessionID(this HttpContext httpContext) => new Guid(httpContext.Session.Id);
+    }
+}

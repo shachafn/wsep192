e0380c832bc4a10aab15c0323a84cefe64693417
Shahaf Nahmias <nahmish@post.bgu.ac.il>
2019-05-09 15:43:22 +0300

    Added Extensions Methods for ICollection

---
+++ src/Utils/ICollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+    public static class ICollectionExtensions
+    {
+        public static void AddRange<T>(this ICollection<T> collection, ICollection<T> otherCollection)
+        {
+            foreach (var other in otherCollection)
+                collection.Add(other);
+        }
+    }
+}

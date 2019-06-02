212d23847a848cd8e20d83fe9217b42fa451e90c
Shahaf Nahmias <nahmish@post.bgu.ac.il>
2019-06-03 01:40:59 +0300

    Created Classes To Communicate With External Services

---
+++ Application/src/Infrastructure/ExternalServices/BaseHttpClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Infrastructure.ExternalServices
+{
+    public class BaseHttpClient
+    {
+        protected static HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://cs-bgu-wsep.herokuapp.com/")
+        };
+
+        protected string Post(Dictionary<string,string> values)
+        {
+            var content = new FormUrlEncodedContent(values);
+            var response = client.PostAsync("https://cs-bgu-wsep.herokuapp.com/", content);
+            response.Wait();
+            var body = response.Result.Content.ReadAsStringAsync();
+            body.Wait();
+            return body.Result;
+        }
+    }
+}

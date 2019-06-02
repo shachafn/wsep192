23c27d797de030d5a221d6b133d240a2e28a5987
Tom Marzea <tommarz@post.bgu.ac.il>
2019-06-03 01:48:17 +0300

    Fixed merge build errors and added init file.

--- Application/tests/ATBridge/BridgeImpl.cs
+++ Application/tests/ATBridge/BridgeImpl.cs
@@ -21,7 +21,7 @@ namespace ATBridge
                             new ServiceFacade(
                                 new DomainLayerFacade(
                                     new UserDomain(NullLogger<UserDomain>.Instance),
-                                    new DomainLayerFacadeVerifier(),
+                                    new DomainLayerFacadeVerifier(NullLogger<DomainLayerFacadeVerifier>.Instance),
                                     NullLogger<DomainLayerFacade>.Instance,
                                     new DefaultExternalServicesManager()
                                 ),

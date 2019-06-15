58fc23be8a1fb3039d001ea118276fce29404ec4
Roee Groiser <roeegro@post.bgu.ac.il>
2019-06-15 20:02:26 +0300

    Adding ShopManagrtAT
    4 tests for doing something according to the privillage.
    5th test - illegal adding of manager.
    6th test- manager trying to do something out of his privillages.
    7th test - manager trying to do something with his 2 privillages

--- tests/AcceptanceBridge/ShopManagerAT.cs
+++ tests/AcceptanceBridge/ShopManagerAT.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using ATBridge;
+using ApplicationCore.Exceptions;
 namespace Tests
 {
     [TestFixture]
@@ -22,24 +23,132 @@ namespace Tests
 
         //GR 5.1 - Commit something by manager should be done according to his permissions.
 
-        /*public static void CommitSomethingWithPermissionAT()
-        {
-            CommitSomethingWithPermissionAT1();
-            CommitSomethingWithPermissionAT2();
-        }
+            //cookie tries to add benCookie and let him manage products
         [Test]
-        public static void CommitSomethingWithPermissionAT1()
+        public static void AddShopManagerAT_1()
         {
-            Assert.Pass();
+            UserAT.GenerateRandoms(out var cookie, out var username, out var password);
+            UserAT.RegisterUser(cookie, username, password);
+            UserAT.LoginUser(cookie, username, password);
+            var shopGuid = Tester.PBridge.OpenShop(cookie, "Name");
+            UserAT.GenerateRandoms(out var benCookie, out var benUsername, out var benPassword);
+            var benGuid = UserAT.RegisterUser(benCookie, benUsername, benPassword);
+            List<bool> onlyManageProducts = { true, false, false, false };
+            Tester.PBridge.AddShopManager(cookie, shopGuid, benGuid, onlyManageProducts);
+            var res = StoreOwnerAT.AddProductToShop(benCookie, shopGuid, "Galaxy S9", "Cellphones", 2000, 10);
+            Assert.AreNotEqual(Guid.Empty, res);
+
         }
+
+        //cookie tries to add benCookie and let him control state of shop.
         [Test]
-        public static void CommitSomethingWithPermissionAT2()
+        public static void AddShopManagerAT_2()
         {
-            Assert.Pass();
+            UserAT.GenerateRandoms(out var cookie, out var username, out var password);
+            UserAT.RegisterUser(cookie, username, password);
+            UserAT.LoginUser(cookie, username, password);
+            var shopGuid = Tester.PBridge.OpenShop(cookie, "Name");
+
+            UserAT.GenerateRandoms(out var benCookie, out var benUsername, out var benPassword);
+            var benGuid = UserAT.RegisterUser(benCookie, benUsername, benPassword);
+            List<bool> onlyControlShopState = { false, true, false, false };
+            Tester.PBridge.AddShopManager(cookie, shopGuid, benGuid, onlyManageProducts);
+            Assert.NotThrows<NoPrivillgesException>(() => Tester.PBridge.CloseShop(benGuid, shopGuid));
         }
-        public static void RunShopManagerAT()
+
+        //cookie tries to add benCookie and let him ManagePolicies.
+        [Test]
+        public static void AddShopManagerAT_3()
         {
-            CommitSomethingWithPermissionAT();
-        }*/
+            UserAT.GenerateRandoms(out var cookie, out var username, out var password);
+            UserAT.RegisterUser(cookie, username, password);
+            UserAT.LoginUser(cookie, username, password);
+            var shopGuid = Tester.PBridge.OpenShop(cookie, "Name");
+            var productGuid = Tester.PBridge.AddProductToShop(cookie, shopGuid, "Galaxy S9", "Cellphones", 2000, 5);
+            UserAT.GenerateRandoms(out var benCookie, out var benUsername, out var benPassword);
+            var benGuid = UserAT.RegisterUser(benCookie, benUsername, benPassword);
+            List<bool> onlyManagePolicies = { false, false, true, false };
+            Tester.PBridge.AddShopManager(cookie, shopGuid, benGuid, onlyManagePolicies);
+            Guid res = Tester.PBridge.AddNewPurchasePolicy(benGuid, shopGuid, "Product purchase policy", productGuid, "<", 2, "Must buy less than 2 Galaxy9");
+            Assert.AreNotEqual(Guid.Empty, res);
+        }
+
+        //cookie tries to add benCookie and let him AppointManagers.
+        [Test]
+        public static void AddShopManagerAT_4()
+        {
+            UserAT.GenerateRandoms(out var cookie, out var username, out var password);
+            UserAT.RegisterUser(cookie, username, password);
+            UserAT.LoginUser(cookie, username, password);
+            var shopGuid = Tester.PBridge.OpenShop(cookie, "Name");
+
+            UserAT.GenerateRandoms(out var benCookie, out var benUsername, out var benPassword);
+            var benGuid = UserAT.RegisterUser(benCookie, benUsername, benPassword);
+            List<bool> onlyAppointManagers = { false, false, false, true };
+            Tester.PBridge.AddShopManager(cookie, shopGuid, benGuid, onlyAppointManagers);
+
+            UserAT.GenerateRandoms(out var lironCookie, out var lironUsername, out var lironPassword);
+            var lironGuid = UserAT.RegisterUser(lironCookie, lironUsername, lironPassword);
+            bool res = Tester.PBridge.AddShopManager(benGuid, shopGuid, lironGuid, onlyAppointManagers);
+            Assert.IsTrue(res);
+        }
+
+        //anotherCookie tries to add benCookie and let him AppointManagers in the shop of cookie.
+        //should fail
+        [Test]
+        public static void AddShopManagerAT_5()
+        {
+            UserAT.GenerateRandoms(out var cookie, out var username, out var password);
+            UserAT.RegisterUser(cookie, username, password);
+            UserAT.LoginUser(cookie, username, password);
+            var shopGuid = Tester.PBridge.OpenShop(cookie, "Name");
+
+            UserAT.GenerateRandoms(out var anothercookie, out var anotherusername, out var anotherpassword);
+            UserAT.RegisterUser(anothercookie, anotherusername, anotherpassword);
+            UserAT.LoginUser(anothercookie, anotherusername, anotherpassword);
+
+            UserAT.GenerateRandoms(out var benCookie, out var benUsername, out var benPassword);
+            var benGuid = UserAT.RegisterUser(benCookie, benUsername, benPassword);
+            List<bool> onlyAppointManagers = { false, false, false, true };
+            Assert.Throws<BadStateException>( ()=>Tester.PBridge.AddShopManager(anothercookie, shopGuid, benGuid, onlyAppointManagers));
+        }
+
+        //cookie tries to add benCookie and let him AppointManagers but ben thinks he is
+        //a dictator and tries to do something else.
+        [Test]
+        public static void AddShopManagerAT_6()
+        {
+            UserAT.GenerateRandoms(out var cookie, out var username, out var password);
+            UserAT.RegisterUser(cookie, username, password);
+            UserAT.LoginUser(cookie, username, password);
+            var shopGuid = Tester.PBridge.OpenShop(cookie, "Name");
+
+            UserAT.GenerateRandoms(out var benCookie, out var benUsername, out var benPassword);
+            var benGuid = UserAT.RegisterUser(benCookie, benUsername, benPassword);
+            List<bool> onlyAppointManagers = { false, false, false, true };
+            Tester.PBridge.AddShopManager(cookie, shopGuid, benGuid, onlyAppointManagers);
+            Assert.Throws<NoPrivillgesException>(() => Tester.PBridge.CloseShop(benGuid, shopGuid) / AddProductToShop(benCookie, shopGuid, "Galaxy S9", "Cellphones", 2000, 10));
+        }
+
+
+        //cookie tries to add benCookie and let him some responsibilties.
+        [Test]
+        public static void AddShopManagerAT_7()
+        {
+            UserAT.GenerateRandoms(out var cookie, out var username, out var password);
+            UserAT.RegisterUser(cookie, username, password);
+            UserAT.LoginUser(cookie, username, password);
+            var shopGuid = Tester.PBridge.OpenShop(cookie, "Name");
+            var productGuid = Tester.PBridge.AddProductToShop(cookie, shopGuid, "Galaxy S9", "Cellphones", 2000, 5);
+            UserAT.GenerateRandoms(out var benCookie, out var benUsername, out var benPassword);
+            var benGuid = UserAT.RegisterUser(benCookie, benUsername, benPassword);
+            List<bool> onlyManagePolicies = { true, false, true, false };
+            Tester.PBridge.AddShopManager(cookie, shopGuid, benGuid, onlyManagePolicies);
+            Guid res = Tester.PBridge.AddNewPurchasePolicy(benGuid, shopGuid, "Product purchase policy", productGuid, "<", 2, "Must buy less than 2 Galaxy9");
+            Assert.AreNotEqual(Guid.Empty, res);
+            var res = StoreOwnerAT.AddProductToShop(benCookie, shopGuid, "Galaxy S10", "Cellphones", 2000, 15);
+            Assert.AreNotEqual(Guid.Empty, res);
+        }
+
     }
 }

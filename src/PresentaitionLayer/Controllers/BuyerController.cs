524e4c8a6d0c0e5d6f7500c4b99528005e1884a6
Hassonbe <hassonbe@post.bgu.ac.il>
2019-06-16 18:59:13 +0300

    a fix

--- src/PresentaitionLayer/Controllers/BuyerController.cs
+++ src/PresentaitionLayer/Controllers/BuyerController.cs
@@ -89,10 +89,11 @@ namespace PresentaitionLayer.Controllers
         {
             IEnumerable<Tuple<ShoppingCart, IEnumerable<ShopProduct>>> bag = _serviceFacade.GetUserBag(new Guid(HttpContext.Session.Id));
             CheckoutModel model = new CheckoutModel(bag);
-            IEnumerable<double> discountPrices = new List<double>();
+            IList<double> discountPrices = new List<double>();
             foreach (Tuple<ShoppingCart, IEnumerable<ShopProduct>> tup in bag)
             {
-                discountPrices.Append(_serviceFacade.GetCartPrice(new Guid(HttpContext.Session.Id), tup.Item1.ShopGuid));
+                var disc = _serviceFacade.GetCartPrice(new Guid(HttpContext.Session.Id), tup.Item1.ShopGuid);
+                discountPrices.Add(disc);
             }
             model.AfterDiscount = discountPrices;
             return View(model);

ebc35cf8bb64c91c64af9ef73328dff62e15dffe
Shahaf Nahmias <nahmish@post.bgu.ac.il>
2019-06-21 01:09:08 +0300

    Fixed Indentations

--- src/ServiceLayer/SystemInitializer.cs
+++ src/ServiceLayer/SystemInitializer.cs
@@ -19,7 +19,6 @@ namespace ServiceLayer
         private IUnitOfWork _unitOfWork;
         ILogger<SystemInitializer> _logger;
         public SystemInitializer(IServiceFacade facade, IUnitOfWork unitOfWork, ILogger<SystemInitializer> logger)
-
         {
             _unitOfWork = unitOfWork;
             _results = new Dictionary<string, Guid>();
@@ -29,7 +28,6 @@ namespace ServiceLayer
 
         public void InitSystemWithFile()
         {
-
             List<Opertaion> jsonList = readJsonFromInitFile();
 
             var session = _unitOfWork.Context.StartSession();
@@ -54,97 +52,61 @@ namespace ServiceLayer
 
 
         private void runOperations(List<Opertaion> opertaions)
-
         {
-
             int i = 1;
 
             foreach (Opertaion op in opertaions)
-
             {
-
                 runOperation(op, i);
-
                 i++;
-
             }
-
         }
 
-
-
         private void runOperation(Opertaion op, int i)
-
         {
-
             switch (op.operationName.ToLower())
-
             {
-
                 case "register":
-
                     register(op, i);
-
                     break;
 
                 case "admin":
-
                     admin(op);
-
                     break;
 
                 case "openshop":
-
                     openshop(op, i);
-
                     break;
 
                 case "addproduct":
-
                     addproduct(op);
-
                     break;
 
                 default:
-
                     throw new Exception("bad init operation");
-
             }
-
         }
 
 
 
         private void addproduct(Opertaion op)
-
         {
-
             if (op.args.Count != 6)
-
             {
-
                 throw new Exception("wrong agument number in system init register");
-
             }
 
             var userId = _results[op.args[0]];
-
             var shopId = _results[op.args[1]];
-
             var name = op.args[2];
-
             var category = op.args[3];
-
             var price = float.Parse(op.args[4]);
-
             var quantity = int.Parse(op.args[5]);
-
             ShopProduct product = new ShopProduct(new Product(name, category), price, quantity);
 
             var shop = _unitOfWork.ShopRepository.FindByIdOrNull(shopId);
             shop.ShopProducts.Add(product);
             _unitOfWork.ShopRepository.Update(shop);
-
         }
 
         public bool InitSystem()
@@ -162,115 +124,68 @@ namespace ServiceLayer
         }
 
         private void openshop(Opertaion op, int i)
-
         {
-
             if (op.args.Count != 2)
-
             {
-
                 throw new Exception("wrong agument number in system init register");
-
             }
 
             var userId = _results[op.args[0]];
-
             var shop = new Shop(userId, op.args[1]);
 
-            //ShopsOwned.Add(shop);
-
             _unitOfWork.ShopRepository.Add(shop);
-
             _results.Add("r" + i, shop.Guid);
-
         }
 
 
 
         private void admin(Opertaion op)
-
         {
-
             if (op.args.Count != 1)
-
             {
-
                 throw new Exception("wrong agument number in system init register");
-
             }
-
             var userId = _results[op.args[0]];
 
-           var user = _unitOfWork.BaseUserRepository.FindByIdOrNull(userId);
+            var user = _unitOfWork.BaseUserRepository.FindByIdOrNull(userId);
             if (user != null)
             {
                 user.IsAdmin = true;
                 _unitOfWork.BaseUserRepository.Update(user);
             }
-
         }
 
-
-
         private void register(Opertaion op, int i)
-
         {
-
             if (op.args.Count != 2)
-
             {
-
                 throw new Exception("wrong agument number in system init register");
-
             }
 
             var userId = _facade.Register(Guid.NewGuid(), op.args[0], op.args[1]);
-
             _results.Add("r" + i, userId);
-
         }
-
-
 
         private List<Opertaion> readJsonFromInitFile()
-
         {
             using (StreamReader r = new StreamReader("../../system_init.json"))
-
             {
-
                 string json = r.ReadToEnd();
-
                 MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
-
                 DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(List<Opertaion>));
-
                 List<Opertaion> operationList = (List<Opertaion>)ser.ReadObject(ms);
-
                 return operationList;
-
             }
-
         }
 
-
-
         [DataContract]
-
         private class Opertaion
-
         {
-
             [DataMember]
-
             internal string operationName;
 
-
-
             [DataMember]
-
             internal List<string> args;
-
         }
 
     }
